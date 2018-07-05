2cadd5f4938747fa07f67661b3faf9cb17516114
Anton Bowden <bowden.anton@gmail.com>
2018-07-05 08:09:51 +0100

    Fetch data from flickr api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetchJsonp from 'fetch-jsonp';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      photos: []
+    }
+    this.loadPhotos = this.loadPhotos.bind(this);
+  }
+
+  loadPhotos() {
+    fetchJsonp("https://api.flickr.com/services/feeds/photos_public.gne?format=json", {
+      jsonpCallbackFunction: "jsonFlickrFeed"
+    })
+    .then(response => response.json())
+    .then(data => this.setState((prevState) => ({ photos: prevState.photos.concat(data.items) })))
+    .catch(err => console.error(err))
+  }
+
+  componentDidMount() {
+    this.loadPhotos();
+  }
+
   render() {
+    console.log(this.state.photos);
     return (
       <div className="App">
         <header className="App-header">
