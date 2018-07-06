0cec56c81077bc1cb429fc431c92a8e4116f0f73
Anton Bowden <bowden.anton@gmail.com>
2018-07-06 10:45:23 +0100

    Add photo descriptions

--- src/PhotoGrid.js
+++ src/PhotoGrid.js
@@ -5,8 +5,11 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Fade from '@material-ui/core/Fade';
-import { extractAuthor } from './utils';
+import { extractAuthor, extractDescription } from './utils';
 import InfiniteScroll from 'react-infinite-scroller';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const styles = theme => ({
   root: {
@@ -39,20 +42,30 @@ function TitlebarGridList(props) {
       loader={<div className="loader" key={'z'}>Loading ...</div>}
     >
       <GridList cellHeight={300} className={classes.gridList} cols={4}>
-        {props.photos.map((photo, index) => (
+        {props.photos.map((photo, index) => {
+          const author = extractAuthor(photo.author);
+          const description = extractDescription(photo.description, index);
+          return (
           <Fade in={true} timeout={{enter: 3000}} key={index}>
             <GridListTile >
               <img src={photo.media.m} alt=""/>
               <GridListTileBar
                 title={photo.title === ' ' || !photo.title ? <a href={photo.link}>Untitled</a> : <a href={photo.link}>{photo.title}</a>}
                 subtitle={
-                  <div><span>by: {<a href={`https://www.flickr.com/people/${photo.author_id}`}>{extractAuthor(photo.author)}</a>}</span>
+                  <div><span>by: {<a href={`https://www.flickr.com/people/${photo.author_id}`}>{author}</a>}</span>
                   <br/><span>Tags: {photo.tags}</span></div>
+                }
+                actionIcon={description &&
+                  <Tooltip id="tooltip-icon" title={<div dangerouslySetInnerHTML={{ __html: description }}/>}>
+                    <IconButton className={classes.icon}>
+                      <InfoIcon />
+                    </IconButton>
+                  </Tooltip>
                 }
               />
             </GridListTile>
           </Fade>
-        ))}
+        )})}
       </GridList>
     </InfiniteScroll>
     </div>
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 export const extractAuthor = (string) => string.slice(string.indexOf('(') + 2, -2);
+
+export const extractDescription = (string) => {
+  const array = string.split('</a></p>');
+  return array.length > 2 ? `${array[1]}</a></p>` : null;
+};
